6d85dc1db867187123b109b751ecdf0d4d041392
LHeczko <lheczko@gmail.com>
2018-01-22 12:53:55 +0100

    AndECHOES

---
+++ Class1.pas
@@ -0,0 +1,31 @@
+﻿namespace ClassLibrary3;
+
+interface
+
+uses
+  java.util,
+  android.app,
+  android.content,
+  android.os,
+  android.util;
+
+type
+  Class1 = public class
+  private
+  protected
+  public
+		method Test();
+  end;
+
+implementation
+
+method Class1.Test;
+{$IF ECHOES}
+var
+	vImage: System.Drawing.Image;
+{$ENDIF}
+begin
+
+end;
+
+end.
